4c8a2d67471eedbe12b3d109f7d2117ec7cef92d
Dipiert <damian_rott2@hotmail.com>
2017-09-29 12:28:05 -0300

    Add files via upload

--- script/insert_gramos.php
+++ script/insert_gramos.php
@@ -1,40 +1,42 @@
-<h1>insert gramos</h1>
-<form action="self" method="POST">
-<input type="submit" value="Enviar"></input>
-</form>
 <?php
-/*
-include 'scripts_conf.php';
-/* include for:
-$servername
-$username
-$password
-$dbname
-$dbprefix
-*/
+
+global $servername,
+$username,
+$password,
+$dbname,
+$dbprefix;
+require 'scripts_conf.php';
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
-    die("La conexión falló: " . $conn->connect_error);
+	die("La conexión falló: " . $conn->connect_error);
 } 
 
-$gramos = calcularGramos();
+function calcularGramosVerificados() {
+	$gramosVerificados = 0;
+	$sql = 'SELECT SUM( peso )
+	FROM pzj13_sistema_iticket
+	WHERE fecha_carga = ' . date('Y-m-d') . '
+	AND verificado = 1';
+	if($result = $conn->query($sql), MYSQLI_USE_RESULT) {
+		$gramosVerificados = $result;
+	}
+	return "$result";
+}
 
-$sql = 'INSERT INTO' . $dbprefix . 'est_general (fecha, gramos) VALUES (null,' . $gramos . ')';
+
+$gramosVerificados = calcularGramosVerificados();
+
+$sql = 'INSERT INTO ' . $dbprefix . 'est_general (fecha, gramos) VALUES (null,' . $gramos . ')';
 //usando "null" como parámetro y current_timestamp en la db
 
 if ($conn->query($sql) === TRUE) {
-    echo "Se creó un nuevo registro exitosamente";
+	echo "Se creó un nuevo registro exitosamente";
 } else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+	echo "Error: " . $sql . "<br>" . $conn->error;
 }
-
 
 $conn->close();
-
-function calcularGramos() {
-	return 100;
-}
 
 ?>
